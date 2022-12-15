bf61f5794ee89074c8e2adc309f7b9f8b9339625
Richard Bausek <richardb@uniform.dev>
2022-12-15 16:41:43 +0000

    disable Contentful and Contentstack enhancers

--- src/lib/enhancers/enhancers.js
+++ src/lib/enhancers/enhancers.js
@@ -1,49 +1,49 @@
 
-import { contentfulEnhancer, contentfulMultiEnhancer, contentfulQueryEnhancer } from './contentful/index.js';
-import { contentstackEnhancer } from './contentstack/index.js';
+// import { contentfulEnhancer, contentfulMultiEnhancer, contentfulQueryEnhancer } from './contentful/index.js';
+// import { contentstackEnhancer } from './contentstack/index.js';
 
 import { EnhancerBuilder } from '@uniformdev/canvas';
-import {
-  CANVAS_CONTENTFUL_PARAMETER_TYPES,
-  CANVAS_CONTENTFUL_MULTI_PARAMETER_TYPES,
-  CANVAS_CONTENTFUL_QUERY_PARAMETER_TYPES,
-} from "@uniformdev/canvas-contentful";
-import {
-  CANVAS_CONTENTSTACK_PARAMETER_TYPES,
-  CANVAS_CONTENTSTACK_MULTI_PARAMETER_TYPES,
-  CANVAS_CONTENTSTACK_QUERY_PARAMETER_TYPES
-} from "@uniformdev/canvas-contentstack";
+// import {
+//   CANVAS_CONTENTFUL_PARAMETER_TYPES,
+//   CANVAS_CONTENTFUL_MULTI_PARAMETER_TYPES,
+//   CANVAS_CONTENTFUL_QUERY_PARAMETER_TYPES,
+// } from "@uniformdev/canvas-contentful";
+// import {
+//   CANVAS_CONTENTSTACK_PARAMETER_TYPES,
+//   CANVAS_CONTENTSTACK_MULTI_PARAMETER_TYPES,
+//   CANVAS_CONTENTSTACK_QUERY_PARAMETER_TYPES
+// } from "@uniformdev/canvas-contentstack";
 import contributerListEnhancer from './components/contributerList.js';
 
 
 export const getEnhancers = () => {
   return new EnhancerBuilder()
-    // Contentful
-    .parameterType(
-      CANVAS_CONTENTFUL_PARAMETER_TYPES,
-      contentfulEnhancer
-    )
-    .parameterType(
-      CANVAS_CONTENTFUL_MULTI_PARAMETER_TYPES,
-      contentfulMultiEnhancer
-    )
-    .parameterType(
-      CANVAS_CONTENTFUL_QUERY_PARAMETER_TYPES,
-      contentfulQueryEnhancer
-    )
-    // Contentstack
-    .parameterType(
-      CANVAS_CONTENTSTACK_PARAMETER_TYPES,
-      contentstackEnhancer
-    )
-    .parameterType(
-      CANVAS_CONTENTSTACK_MULTI_PARAMETER_TYPES,
-      contentstackEnhancer
-    )
-    .parameterType(
-      CANVAS_CONTENTSTACK_QUERY_PARAMETER_TYPES,
-      contentstackEnhancer
-    )
+    // // Contentful
+    // .parameterType(
+    //   CANVAS_CONTENTFUL_PARAMETER_TYPES,
+    //   contentfulEnhancer
+    // )
+    // .parameterType(
+    //   CANVAS_CONTENTFUL_MULTI_PARAMETER_TYPES,
+    //   contentfulMultiEnhancer
+    // )
+    // .parameterType(
+    //   CANVAS_CONTENTFUL_QUERY_PARAMETER_TYPES,
+    //   contentfulQueryEnhancer
+    // )
+    // // Contentstack
+    // .parameterType(
+    //   CANVAS_CONTENTSTACK_PARAMETER_TYPES,
+    //   contentstackEnhancer
+    // )
+    // .parameterType(
+    //   CANVAS_CONTENTSTACK_MULTI_PARAMETER_TYPES,
+    //   contentstackEnhancer
+    // )
+    // .parameterType(
+    //   CANVAS_CONTENTSTACK_QUERY_PARAMETER_TYPES,
+    //   contentstackEnhancer
+    // )
     // Component enhancers
     .component(
       'contributorList',
