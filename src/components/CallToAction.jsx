59c58a2655bd2ba6a78114c3ceb9ab0f101356cd
Richard Bausek <richardb@uniform.dev>
2023-02-03 10:47:44 +0100

    support rendering of project map links

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import { Fragment } from "react";
+import Link from 'next/link';
+import { Fragment } from 'react';
 
 const renderPrimaryCta = (title, link, openInNewTab) => {
   return (
@@ -7,14 +7,14 @@ const renderPrimaryCta = (title, link, openInNewTab) => {
       <Link href={link}>
         <a
           className="flex w-full items-center justify-center rounded-md border border-transparent bg-mango px-8 py-3 text-base font-medium text-aqua-900 hover:bg-mango-200 md:py-4 md:px-10 md:text-lg"
-          target={openInNewTab ? "_blank" : "_self"}
+          target={openInNewTab ? '_blank' : '_self'}
         >
           {title}
         </a>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 const renderSecondaryCta = (title, link, openInNewTab) => {
   return (
@@ -22,23 +22,30 @@ const renderSecondaryCta = (title, link, openInNewTab) => {
       <Link href={link}>
         <a
           className="flex w-full items-center justify-center rounded-md border border-transparent bg-ice px-8 py-3 text-base font-medium text-aqua-900 hover:bg-ice-200 md:py-4 md:px-10 md:text-lg"
-          target={openInNewTab ? "_blank" : "_self"}
+          target={openInNewTab ? '_blank' : '_self'}
         >
           {title}
         </a>
       </Link>
     </div>
-  )
-}
-
-export default function CallToAction({ title='', link='', openInNewTab=false, linkPrefix='', component }) {
-  const variant = component.variant || 'primary'; 
-  const url = linkPrefix + link;
-  const linkCta = variant === 'primary' ? renderPrimaryCta(title, url, openInNewTab) : renderSecondaryCta(title, url, openInNewTab) ;
-  
-  return (
-    <>
-      {linkCta}
-    </>
   );
+};
+
+export default function CallToAction({
+  title = '',
+  link = '',
+  internalLink = {},
+  openInNewTab = false,
+  linkPrefix = '',
+  component,
+}) {
+  const variant = component.variant || 'primary';
+  const url = internalLink.path?.length ? internalLink.path : linkPrefix + link;
+
+  const linkCta =
+    variant === 'primary'
+      ? renderPrimaryCta(title, url, openInNewTab)
+      : renderSecondaryCta(title, url, openInNewTab);
+
+  return <>{linkCta}</>;
 }
