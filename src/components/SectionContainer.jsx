71de57b608297b177c9cc3642eddbff2206c254c
Richard Bausek <richardb@uniform.dev>
2023-03-13 18:43:11 +0100

    fix slots checks

--- src/components/SectionContainer.jsx
+++ src/components/SectionContainer.jsx
@@ -3,7 +3,7 @@ import Section from './Section';
 import Heading from './Heading';
 
 export default function SectionContainer({ title, theme, component }) {
-  const hasHeadingComponent = component.slots.heading?.length ? true : false;
+  const hasHeadingComponent = component.slots?.heading?.length ? true : false;
   const titleComponent = hasHeadingComponent ? (
     <UniformSlot name="heading" emptyPlaceholder={null} />
   ) : null;
