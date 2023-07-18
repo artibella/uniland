714d484856494f4c2732e47de09b8e76e0dbb7bf
Richard Bausek <richardb@uniform.dev>
2023-07-18 18:20:06 +0200

    allow passing in JSON to Lottie Animation

--- src/components/LottieAnimation.jsx
+++ src/components/LottieAnimation.jsx
@@ -1,33 +1,34 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react';
 
-export default function LottieAnimation ( {
-  id="",
-  src="", 
-  autoplay = false, 
-  controls = false, 
-  loop=true, 
+export default function LottieAnimation({
+  id = '',
+  src = '',
+  jsonSrc = '',
+  autoplay = false,
+  controls = false,
+  loop = true,
   hover = false,
-  width = "300px",
-  height = "300px"
-  } ) {
+  width = '300px',
+  height = '300px',
+}) {
   const ref = useRef(null);
   React.useEffect(() => {
-    import("@lottiefiles/lottie-player");
+    import('@lottiefiles/lottie-player');
   });
 
   return (
     <div className="lottie-animation">
-        <lottie-player
-          id={id}
-          ref={ref}
-          autoplay={Boolean(autoplay) ? 'autoplay' : undefined} 
-          controls={Boolean(controls) ? 'controls' : undefined}
-          loop={Boolean(loop) ? 'loop' : undefined}
-          hover={Boolean(hover) ? 'hover' : undefined}
-          mode="normal"
-          src={src}
-          style={{ width, height }}
-        ></lottie-player>
+      <lottie-player
+        id={id}
+        ref={ref}
+        autoplay={Boolean(autoplay) ? 'autoplay' : undefined}
+        controls={Boolean(controls) ? 'controls' : undefined}
+        loop={Boolean(loop) ? 'loop' : undefined}
+        hover={Boolean(hover) ? 'hover' : undefined}
+        mode="normal"
+        src={jsonSrc || src}
+        style={{ width, height }}
+      ></lottie-player>
     </div>
   );
 }
