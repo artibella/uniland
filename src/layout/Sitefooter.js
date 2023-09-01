6f25de5dacdad4410d3a84649817e36c193ead29
Richard Bausek <richardb@uniform.dev>
2023-09-01 13:31:27 +0200

    add Context Devtools

--- src/layout/Sitefooter.js
+++ src/layout/Sitefooter.js
@@ -1,15 +1,33 @@
-import { Footer } from "react-daisyui"
+import { Footer } from 'react-daisyui';
+import { ToggleEmbeddedContextDevTools } from '@uniformdev/context-devtools';
+import getConfig from 'next/config';
 
 export default function Sitefooter({ children }) {
+  const {
+    publicRuntimeConfig: { projectId, canvasApiHost, canvasApiKey },
+  } = getConfig();
 
   return (
     <div className="site-footer mt-32">
       <Footer className="p-10 bg-dark text-base-content">
         <div>
-          <Footer.Title>This is a ficticious website for demoing proposes only</Footer.Title>
-             <a className="link link-hover" href="https://uniform.dev">Learn more about Uniform</a>
+          <Footer.Title>
+            This is a ficticious website for demoing proposes only
+          </Footer.Title>
+          <a className="link link-hover" href="https://uniform.dev">
+            Learn more about Uniform
+          </a>
+        </div>
+        <div>
+          <ToggleEmbeddedContextDevTools
+            initialSettings={{
+              apiHost: canvasApiHost,
+              apiKey: canvasApiKey,
+              projectId: projectId,
+            }}
+          />
         </div>
       </Footer>
     </div>
-  )
+  );
 }
