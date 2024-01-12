e45e5b875ac816a73e59b6ec65838a27233b6094
Richard Bausek <richardb@uniform.dev>
2024-01-12 12:31:20 +0100

    update link list rendering

--- src/components/LinkListItem.jsx
+++ src/components/LinkListItem.jsx
@@ -1,11 +1,20 @@
-export default function LinklistItem({ title = "", description = "", linkPrefix="", link = "" }) {
-  const url = linkPrefix + link;
+export default function LinklistItem({
+  title = '',
+  description = '',
+  link = '',
+}) {
+  const url = link.path;
   return (
-    <a href={url} className="block p-8 border-y-2 border-collapse hover:underline">
+    <a
+      href={url}
+      className="block p-8 border-y-2 border-collapse hover:underline"
+    >
       <div className="md:grid md:grid-cols-12 md:gap-12">
-        <h4 className="font-bold font-serif text-3xl mb-4 md:mb-0 md:text-4xl md:col-span-5">{title}</h4>
+        <h4 className="font-bold font-serif text-3xl mb-4 md:mb-0 md:text-4xl md:col-span-5">
+          {title}
+        </h4>
         <p className="md:text-xl md:col-span-7">{description}</p>
       </div>
     </a>
-  )
+  );
 }
