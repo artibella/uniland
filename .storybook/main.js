248e063273563a5626002b648d5f778a9346d082
Richard Bausek <richardb@uniform.dev>
2024-02-27 13:28:21 +0100

    Storybook update

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,28 +4,17 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   /** Expose public folder to storybook as static */
   staticDirs: ['../public'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    {
-      /**
-       * Fix Storybook issue with PostCSS@8
-       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
-       */
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
+    '@storybook/addon-styling-webpack',
   ],
-  core: {
-    builder: 'webpack5',
-  },
-  webpackFinal: (config) => {
+
+  webpackFinal: config => {
     /**
      * Add support for alias-imports
      * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
@@ -45,5 +34,14 @@ module.exports = {
     ];
 
     return config;
+  },
+
+  docs: {
+    autodocs: true,
+  },
+
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
 };
