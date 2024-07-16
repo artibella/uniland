224f3d0e6e8d6e62bb6d4b839a84d84c5aa38092
Richard Bausek <richardb@uniform.dev>
2024-07-16 16:28:03 +0200

    fix className issues

--- src/components/GenericHero.jsx
+++ src/components/GenericHero.jsx
@@ -29,9 +29,9 @@ export default function GenericHero({
   const useDefaultHeading = !Array.isArray(component.slots?.heading);
 
   return (
-    <section class="bg-white dark:bg-gray-900">
-      <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
+    <section className="bg-white dark:bg-gray-900">
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
           {useDefaultHeading ? (
             <Heading title={title} tagName="h2" />
           ) : (
@@ -50,7 +50,7 @@ export default function GenericHero({
             <UniformSlot name="ctas" />
           </div>
         </div>
-        <div class="block mt-8 lg:mt-0 lg:col-span-5 lg:flex">{image}</div>
+        <div className="block mt-8 lg:mt-0 lg:col-span-5 lg:flex">{image}</div>
       </div>
     </section>
   );
