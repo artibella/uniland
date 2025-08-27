8bac95e94c54a72baa247551b4802d5997ef5ac2
Richard Bausek <richardb@uniform.dev>
2025-08-27 20:01:25 +0200

    fix nav

--- src/layout/Siteheader.jsx
+++ src/layout/Siteheader.jsx
@@ -4,7 +4,7 @@ import NavBar from '../components/Navbar';
 export default function Siteheader({ children }) {
   return (
     <div className="site-header relative bg-white py-8 px-4 sm:px-8">
-      <div className="container mx-auto flex">
+      <div className="container mx-auto flex justify-between items-center">
         <div className="site-branding">
           <Link href="/">
             <span className="text-5xl font-bold text-left font-serif">
@@ -13,30 +13,26 @@ export default function Siteheader({ children }) {
           </Link>
         </div>
         <NavBar>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between items-center h-16">
-              {/* Navigation links */}
-              <div className="flex items-baseline space-x-8">
-                <a
-                  href="/en-US"
-                  className="font-serif text-gray-700 hover:text-gray-900 px-3 py-2 text-lg font-medium transition-colors duration-200"
-                >
-                  Home
-                </a>
-                <a
-                  href="/en-US/blog"
-                  className="font-serif text-gray-700 hover:text-gray-900 px-3 py-2 text-lg font-medium transition-colors duration-200"
-                >
-                  Blog
-                </a>
-                <a
-                  href="/en-US/shop"
-                  className="font-serif text-gray-700 hover:text-gray-900 px-3 py-2 text-lg font-medium transition-colors duration-200"
-                >
-                  Shop
-                </a>
-              </div>
-            </div>
+          {/* Navigation links */}
+          <div className="flex items-center space-x-8">
+            <a
+              href="/en-US"
+              className="font-serif text-gray-700 hover:text-gray-900 px-3 py-2 text-lg font-bold underline transition-colors duration-200"
+            >
+              Home
+            </a>
+            <a
+              href="/en-US/blog"
+              className="font-serif text-gray-700 hover:text-gray-900 px-3 py-2 text-lg font-bold underline transition-colors duration-200"
+            >
+              Blog
+            </a>
+            <a
+              href="/en-US/shop"
+              className="font-serif text-gray-700 hover:text-gray-900 px-3 py-2 text-lg font-bold underline transition-colors duration-200"
+            >
+              Shop
+            </a>
           </div>
         </NavBar>
       </div>
