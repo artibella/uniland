e10279674a66df05000873fff78fcbca5560861a
Richard Bausek <richardb@uniform.dev>
2025-08-28 10:32:22 +0200

    add layout folder to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layout/**/*.{js,ts,jsx,tsx}',
     './src/compositions/**/*.{js,ts,jsx,tsx}',
     'node_modules/daisyui/dist/**/*.js',
     'node_modules/react-daisyui/dist/**/*.js',
